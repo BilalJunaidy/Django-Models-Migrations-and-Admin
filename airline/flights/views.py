e785684b3d71ed819df2d793309938e170a9a6e7
Bilal Junaidy <bilal.junaidy@gmail.com>
2020-11-14 17:31:52 -0500

    users app and flight app updated

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Flight, Airport 
+from .models import Flight, Airport, Passenger
 
 # Create your views here.
 
@@ -16,18 +16,20 @@ def flight(request, flight_id):
 
     flight = Flight.objects.get(id=flight_id)
     passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
     return render(request, "flights/flight.html", {
         "flight": flight, 
-        "passengers": passengers
+        "passengers": passengers, 
+        "non_passengers": non_passengers
     })
 
-def book(requet, flight_id):
+def book(request, flight_id):
     if request.method == "POST":
         flight = Flight.objects.get(pk=flight_id)
-        passenger = Passenger.objects.get(pk = int(request.POST["passenger"]))
-        passenger.flights.add(flight)
+        if request.POST["passenger"] == "empty":
+            return HttpResponseRedirect(reverse("flight", args=[flight.id]))
+        else:
+            passenger = Passenger.objects.get(pk = int(request.POST["passenger"]))
+            passenger.flights.add(flight)
 
-        return HttpResponseRedirect(reverse("flight", args=(flight.id)))
-
-    else:
-        return render(request, "flights\book.html")
+            return HttpResponseRedirect(reverse("flight", args=[flight.id]))
